156bceb374a99e675c6bfa64db86adfb278d3f97
rjo6615 <robertjobernier@gmail.com>
2023-02-27 21:23:34 -0500

    fixed typo on nav bar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -11,7 +11,7 @@ function NavbarComponent() {
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
             <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: '100px' }} navbarScroll>
-              <Nav.Link href="/create-routines">Create Routine</Nav.Link>
+              <Nav.Link href="/create-routine">Create Routine</Nav.Link>
               <Nav.Link href="/routines">Routines</Nav.Link>
               <Nav.Link href="/logout">Logout</Nav.Link>
             </Nav>
