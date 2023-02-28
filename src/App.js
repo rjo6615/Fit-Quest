85c8eda650db9b84e6e9daf06f64fcd36ae2c640
rjo6615 <robertjobernier@gmail.com>
2023-02-27 21:07:18 -0500

    added carousel and button

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router,  Route,  Routes,  Navigate} from "react-router-dom";
 // import CreateRoutine from "./components/CreateRoutine/CreateRoutine";
 // import Routines from "./components/Routines/Routines";
+import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
 // import Footer from "./components/Footer/Footer";
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -13,10 +14,10 @@ function App() {
     <Router>
         <Navbar />
         <Routes>
-          {/* <Route path="/" element={<Home />} />
-          <Route path="/create-routine" element={<CreateRoutine />} />
-          <Route path="/routine" element={<Routines />} />
-          <Route path="*" element={<Navigate to="/"/>} /> */}
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/create-routine" element={<CreateRoutine />} />
+          <Route path="/routine" element={<Routines />} /> */}
+          <Route path="*" element={<Navigate to="/"/>} />
         </Routes>
         {/* <Footer /> */}
     </Router>
