eb2f9c03731024b75cd4b426e0ae022d50745e90
rjo6615 <robertjobernier@gmail.com>
2023-02-28 22:35:01 -0500

    set up backend with a get all route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router,  Route,  Routes,  Navigate} from "react-router-dom";
 import CreateRoutine from "./components/CreateRoutine/CreateRoutine";
-// import Routines from "./components/Routines/Routines";
+import Routines from "./components/Routines/Routines";
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
@@ -18,7 +18,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/create-routine" element={<CreateRoutine />} />
-          {/* <Route path="/routine" element={<Routines />} /> */}
+          <Route path="/routines" element={<Routines />} />
           <Route path="*" element={<Navigate to="/"/>} />
         </Routes>
         <Footer />
