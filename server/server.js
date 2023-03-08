fb83d4f6b94202ea9330f00909eab918ab7a6645
rjo6615 <robertjobernier@gmail.com>
2023-03-08 12:56:14 -0500

    trying to get heroku to work

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(express.static('client/build'));
 
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 
