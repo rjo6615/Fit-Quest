8964ff75a8396394cf74993418d9ac05e858308b
rjo6615 <robertjobernier@gmail.com>
2023-03-07 23:50:55 -0500

    trying to get heroku to work

--- server/server.js
+++ server/server.js
@@ -6,8 +6,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/routines"));
-// const path = require('path')
-// app.use('/', express.static(path.join(__dirname, '../client/src')));
+const path = require('path')
+app.use('/', express.static(path.join(__dirname, '../client/src')));
 
 app.use('/login', (req, res) => {
   res.send({
