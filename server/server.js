13444e954c662c69a72a3b55f10916f1f80fcd80
rjo6615 <robertjobernier@gmail.com>
2023-03-08 02:33:20 -0500

    trying to get heroku to work

--- server/server.js
+++ server/server.js
@@ -8,8 +8,8 @@ app.use(express.json());
 app.use(require("./routes/routines"));
 const path = require('path')
 app.use(express.static('client/build'));
-app.get('*', (req, res) => 
-res.sendFile(path.join(__dirname, "./client/build/index.html")));
+// app.get('*', (req, res) => 
+// res.sendFile(path.join(__dirname, "./client/build/index.html")));
 
 
 app.use('/login', (req, res) => {
